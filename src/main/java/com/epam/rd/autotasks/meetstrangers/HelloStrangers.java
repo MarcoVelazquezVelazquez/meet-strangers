0b83641b58b39172c7cce21e09810237472cf5d2
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-27 14:43:19 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
+++ src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
@@ -1,10 +1,28 @@
 package com.epam.rd.autotasks.meetstrangers;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloStrangers {
     public static void main(String[] args) throws IOException {
         //Write a program, asks for a number - amount of strangers to meet.
         //Then reads stranger names line by line and prints line by line "Hello, ...".
+
+        Scanner input = new Scanner(System.in);
+        /*System.out.println("Asks for a number - amount of strangers to meet");*/
+        int amountOfStrangersToMeet = input.nextInt();
+
+        if (amountOfStrangersToMeet < 0) {
+            System.out.println("Seriously? Why so negative?");
+        } else if (amountOfStrangersToMeet == 0) {
+            System.out.println("Oh, it looks like there is no one here");
+        } else {
+            //input.nextLine();
+            for (int i = 0; i < amountOfStrangersToMeet; i++) {
+                String strangersNames = input.nextLine();
+                System.out.println("Hello, " + strangersNames);
+            }
+        }
     }
 }
+
